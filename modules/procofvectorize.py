5a5c6b3b5900b55372cf22b92d1fb534953043a4
Madbook <madbook@keemail.me>
2024-07-24 22:05:28 +0800

    Add files via upload

--- modules/procofvectorize.py
+++ modules/procofvectorize.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 # ----------------------------------------
+import re
 import json
 import tiktoken
 from modules.globals import *
@@ -7,40 +8,46 @@ from modules.globals import *
 # ----------------------------------------
 # Split the content of document into multiple pieces(chunk)
 # ----------------------------------------
-def Chunk( TokenLength, Content ):
-	Result = []
+def Chunk( SizeofChunk, Content ):
 
-	SumofLength = 0
-	ThisChunk = ''
+	# Give up if wihtou proper arguments !
+	if not isinstance( Content, str ) : return False
+	Remain = len( Content )
+	if Remain == 0 or SizeofChunk < MIN_TOKENLENGTH : return False
+
+	LengthofOverlap = SizeofChunk // 3 # about 1/3
+
+	Chunks = []
+
+	# Chunk, means splitting the content of document to multi parts, but, how to avoid the mistake of sundering a word while splitting? The first trick is splitting all lines, because each line is a natural separator which splits content without cut any words incorrectly.
+	Lines = Content.splitlines()
+
+	for ThisLine in Lines :
+
+		Since, Till, Remain = 0, SizeofChunk-1, len( ThisLine )
+		Endding = Remain -1 # index of the lastest character of string
 	
-	Encoder = tiktoken.get_encoding("cl100k_base")
-	
-	Lines = Content.splitlines()  # suppose each line ends with CRLF 
-	for Line in Lines:
-		# Line = Line.replace(' ', '')
-		LineLength = len( Encoder.encode(Line) )
+		while Remain > 0 :
+			if Remain <= SizeofChunk :
+				Chunks.append( ThisLine[Since:] )
+				Remain = 0
+			else:
+				Anchor = Till # save it because this pointer will be moved.
 
-		if LineLength > TokenLength :
-			NubmberofParts = - ( -LineLength // TokenLength )
-			for i in range( NubmberofParts ):
-				Since = i * NubmberofParts
-				Till = Since + TokenLength
-				# Don't split a word
-				while not Line[Since:Till].rstrip().isspace():
-					Since, Till = Since+1, Till+1
-					if Since >= LineLength : break
-				Result.append( Line[Since:Till] )
+				# The second trick is, find out the lastest space, and then take its former content.
+				while Till > Since :
+					if ThisLine[Till] == chr(32) : break
+					else : Till -= 1
 
-		if SumofLength + LineLength <= TokenLength:
-			SumofLength += LineLength+1
-			ThisChunk += Line+'\n'
-		else:
-			Result.append(ThisChunk)
-			SumofLength, ThisChunk = LineLength, Line		
+				if Till == Since : Till = Anchor # no space? copy all!
+				
+				Chunk.append( ThisLine[Since:Till+1] ) # don't miss the space.
 
-	if len( ThisChunk ) > 0 : Result.append( ThisChunk )
+				Remain -= ( Till - Since +1 )
+				# Move the pointer! Be aware that Since and Till may be same.
+				Since, Till = Till+1, min( Till+SizeofChunk, Endding )
 
-	return [_ for _ in Result if _.strip()]
+	return Chunks
 
 # ----------------------------------------
 def GetContentofDocument( TokenLength, NameofDocument ):
